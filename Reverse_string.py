80bb6a8e5ecf026169758b256e611d5d84dc2b22
hemilshahh <94670911+hemilshahh@users.noreply.github.com>
2023-07-06 17:13:44 +0530

    Update Reverse_string.py

--- Reverse_string.py
+++ Reverse_string.py
@@ -1,2 +1,5 @@
-word=input("Enter a word: ")
-print(word[::-1])
+word=list(input("Enter a word: "))
+reverse=[]
+for i in range(0,len(word)):
+    reverse.append(word.pop())
+print("".join(reverse)) 
