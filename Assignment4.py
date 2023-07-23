49209f7fc1b8f2462825d75ba62b4b88ac9ac18f
hemilshahh <94670911+hemilshahh@users.noreply.github.com>
2023-07-23 20:17:58 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,15 @@
+#1
+add25=lambda x: x+25
+print(add25(10))
+
+
+#2
+l=[1, 2, 3, 4, 5, 6, 7]
+l1=list(map(lambda x: x*3,l))
+print(l1)
+
+
+#3
+l=[4, 5, 2, 9]
+l1=list(map(lambda x:x**2,l))
+print(l1)
